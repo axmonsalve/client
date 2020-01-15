75b8b3fa6f0e5e51fefc7d55954b590cdd2a85df
Axel <axel.monsalve.ponce@gmail.com>
2020-01-15 02:21:47 -0300

    State and functions for Login component

--- src/components/auth/Login.js
+++ src/components/auth/Login.js
@@ -1,33 +1,75 @@
-import React from 'react';
+import React, { useState } from "react";
+import  { Link } from 'react-router-dom';
 
 const Login = () => {
+  //State para inciar sesión
+  const [user, setUser] = useState({
+    email: "",
+    password: ""
+  });
 
-    const onChange = () => {
+  //Extraer de usuario
+  const { email, password } = user;
 
-    }
+  //Colocamos los valores de los inputs al state mientras van cambiando
+  const onChange = e => {
+    setUser({
+      ...user,
+      [e.target.name]: e.target.value
+    });
+  };
 
-    return (
-        <div className="form-usuario">
-            <div className="contenedor-form sombra-dark">
-                <h1>Iniciar Sesión</h1>
-                <form>
-                    <div className="campo-form">
-                        <label htmlFor="email">Email</label>
-                        <input type="email" name="email" id="email" placeholder="Tu nombre" onChange={onChange}/>
-                    </div>
+  //Cuando el usuario quiere iniciar sesión
+  const onSubmit = e => {
+      e.preventDefault();
 
-                    <div className="campo-form">
-                        <label htmlFor="password">Password</label>
-                        <input type="password" name="password" id="password" placeholder="Tu password" onChange={onChange}/>
-                    </div>
+      //Validar que no haya campos vacíos
 
-                    <div className="campo-form">
-                        <input type="submit" value="Iniciar Sesión" className="btn btn-primario btn-block"/>
-                    </div>
-                </form>
-            </div>
-        </div>
-    );
+      
+      //Pasarlo al action
+  }
+
+  return (
+    <div className="form-usuario">
+      <div className="contenedor-form sombra-dark">
+        <h1>Iniciar Sesión</h1>
+        <form onSubmit={onSubmit}>
+          <div className="campo-form">
+            <label htmlFor="email">Email</label>
+            <input
+              type="email"
+              value={email}
+              name="email"
+              id="email"
+              placeholder="Tu nombre"
+              onChange={onChange}
+            />
+          </div>
+
+          <div className="campo-form">
+            <label htmlFor="password">Password</label>
+            <input
+              type="password"
+              value={password}
+              name="password"
+              id="password"
+              placeholder="Tu password"
+              onChange={onChange}
+            />
+          </div>
+
+          <div className="campo-form">
+            <input
+              type="submit"
+              value="Iniciar Sesión"
+              className="btn btn-primario btn-block"
+            />
+          </div>
+        </form>
+        <Link to={'/new-account'} className="enlace-cuenta">Obtener una cuenta</Link>
+      </div>
+    </div>
+  );
 };
 
 export default Login;
