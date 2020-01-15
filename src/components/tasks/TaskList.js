5e7a1756f5c728b90a5031a2d2c86cd8fecbe5da
Axel <axel.monsalve.ponce@gmail.com>
2020-01-15 17:30:52 -0300

    Finishing design part of tasks

--- src/components/tasks/TaskList.js
+++ src/components/tasks/TaskList.js
@@ -1,10 +1,35 @@
-import React from 'react';
+import React, { Fragment } from 'react';
+import Task from './Task';
 
 const TaskList = () => {
+
+    const tasks = [
+        {name: 'Elegir plataforma', stateTask: true},
+        {name: 'Elegir colores', stateTask: false},
+        {name: 'Elegir plataformas de pago', stateTask: false},
+        {name: 'Elegir hosting', stateTask: true},
+    ]
+
     return (
-        <div>
-            
-        </div>
+        <Fragment>
+            <h2>Proyecto: Tienda Virtual</h2>
+            <ul className="listado-tareas">
+                {tasks.length === 0 
+                ? (
+                    <li className="tarea"><p>No hay tareas</p></li>
+                ): tasks.map(task => (
+                    <Task 
+                        task={task}
+                    />
+                ))
+                }
+            </ul>
+
+            <button
+                type="button"
+                className="btn btn-eliminar"
+            >Eliminar proyecto &times;</button>
+        </Fragment>
     );
 };
 
