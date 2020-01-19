591fd47bbad98fdddcf59251cd18a6820a776510
Axel <axel.monsalve.ponce@gmail.com>
2020-01-19 03:02:39 -0300

    Creating a function that obtains the tasks of the Project

--- src/components/projects/Project.js
+++ src/components/projects/Project.js
@@ -2,13 +2,25 @@ import React, { useContext } from "react";
 
 import ProjectContext from "../../context/projects/projectContext";
 
+import TaskContext from "../../context/Tasks/taskContext";
+
 const Project = ({ project }) => {
   //Obtener el state de proyectos
   const projectsContext = useContext(ProjectContext);
   const { actualProject } = projectsContext;
+
+  //Obtener la/s funcion/es del context de tarea
+  const tasksContext = useContext(TaskContext);
+  const { getTaskFn } = tasksContext;
+
+  //Funcion para agregar el proyecto actual
+  const selectProject = id => {
+    actualProject(id); //Fijando el proyecto actual
+    getTaskFn(id); //Fijando las tareas cuando el usuario de click en el proyecto
+  }
   return (
     <li>
-      <button type="button" className="btn btn-blank" onClick={() => actualProject(project.id)}>
+      <button type="button" className="btn btn-blank" onClick={() => selectProject(project.id)}>
         {project.name}
       </button>
     </li>
--- src/context/Tasks/taskState.js
+++ src/context/Tasks/taskState.js
@@ -1,39 +1,51 @@
-import React, { useReducer } from 'react';
+import React, { useReducer } from "react";
 
-import TaskContext from './taskContext';
-import TaskReducer from './taskReducer';
+import TaskContext from "./taskContext";
+import TaskReducer from "./taskReducer";
+
+import { TASKS_PROJECT } from "../../types";
 
 const TaskState = props => {
-    const initialState = {
-        tasks: [
-            { name: "Elegir plataforma", stateTask: true, projectId: 1 },
-            { name: "Elegir colores", stateTask: false, projectId: 2  },
-            { name: "Elegir plataformas de pago", stateTask: false, projectId: 3  },
-            { name: "Elegir hosting", stateTask: true, projectId: 4  },
-            { name: "Elegir plataforma", stateTask: true, projectId: 2 },
-            { name: "Elegir colores", stateTask: false, projectId: 3  },
-            { name: "Elegir plataformas de pago", stateTask: false, projectId: 1  },
-            { name: "Elegir hosting", stateTask: true, projectId: 4  },
-            { name: "Elegir plataforma", stateTask: true, projectId: 3 },
-            { name: "Elegir colores", stateTask: false, projectId: 1  },
-            { name: "Elegir plataformas de pago", stateTask: false, projectId: 2  },
-            { name: "Elegir hosting", stateTask: true, projectId: 4  }
-          ],
-    }
+  const initialState = {
+    tasks: [
+      { name: "Elegir plataforma", stateTask: true, projectId: 1 },
+      { name: "Elegir colores", stateTask: false, projectId: 2 },
+      { name: "Elegir plataformas de pago", stateTask: false, projectId: 3 },
+      { name: "Elegir hosting", stateTask: true, projectId: 4 },
+      { name: "Elegir plataforma", stateTask: true, projectId: 2 },
+      { name: "Elegir colores", stateTask: false, projectId: 3 },
+      { name: "Elegir plataformas de pago", stateTask: false, projectId: 1 },
+      { name: "Elegir hosting", stateTask: true, projectId: 4 },
+      { name: "Elegir plataforma", stateTask: true, projectId: 3 },
+      { name: "Elegir colores", stateTask: false, projectId: 1 },
+      { name: "Elegir plataformas de pago", stateTask: false, projectId: 2 },
+      { name: "Elegir hosting", stateTask: true, projectId: 4 }
+    ]
+  };
 
-    //Crear Dispatch y el state
-    const [state, dispatch] = useReducer(TaskReducer, initialState);
+  //Crear Dispatch y el state
+  const [state, dispatch] = useReducer(TaskReducer, initialState);
 
-    return(
-        <TaskContext.Provider
-            value={{
-                tasks: state.tasks
-            }}
-        >
-            {props.children}
-        </TaskContext.Provider>
-    )
+  //********FUNCIONES**********//
 
-}
+  //Obtener las tareas del proyecto
+  const getTaskFn = projectId => {
+    dispatch({
+      type: TASKS_PROJECT,
+      payload: projectId
+    });
+  };
+
+  return (
+    <TaskContext.Provider
+      value={{
+        tasks: state.tasks,
+        getTaskFn
+      }}
+    >
+      {props.children}
+    </TaskContext.Provider>
+  );
+};
 
 export default TaskState;
