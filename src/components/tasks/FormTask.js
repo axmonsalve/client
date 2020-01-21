9612a571ebaa6977f4ad42cd4e37d6b2b260d291
Axel <axel.monsalve.ponce@gmail.com>
2020-01-20 23:14:07 -0300

    Insert tasks in the Current Project

--- src/components/tasks/FormTask.js
+++ src/components/tasks/FormTask.js
@@ -1,40 +1,73 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from "react";
 import ProjectContext from "../../context/projects/projectContext";
+import TaskContext from "../../context/Tasks/taskContext";
 
 const FormTask = () => {
-    //Para extraer si algun proyecto esta activo
+  //Para extraer si algun proyecto esta activo
   const projectsContext = useContext(ProjectContext);
   const { selectedProject } = projectsContext;
 
-   //Si no hay proyecto seleccionado
-   if(!selectedProject)return null;
+  //Obtener la/s funcion/es del context de tarea
+  const tasksContext = useContext(TaskContext);
+  const { addTaskFn } = tasksContext;
 
-   const onSubmitAddTask = e => {
-       e.preventDefault();
+  //State
+  const [task, getTask] = useState({
+    nametask: ""
+  });
 
-       //Validar
+  const { nametask } = task;
 
-       //Pasar la validación
+  //Si no hay proyecto seleccionado
+  if (!selectedProject) return null;
 
-       //Agregar la tarea al state de tareas
+  const hadleChange = e => {
+    getTask({
+        ...task,
+        [e.target.name]: e.target.value
+    })
+  };
 
-       //reiniciar el form
-   }
+  const [actualProject] = selectedProject;
 
-    return (
-        <div className="formulario">
-            <form
-                onSubmit={onSubmitAddTask}
-            >
-                <div className="contenedor-input">
-                    <input type="text" className="input-text" placeholder="Nombre de la tarea" name="name"/>
-                </div>
-                <div className="contenedor-input">
-                    <input type="submit" className="btn tbn-primario btn-submit btn-block" value="Agregar tarea"/>
-                </div>
-            </form>
+  const onSubmitAddTask = e => {
+    e.preventDefault();
+
+    //Validar
+
+    //Pasar la validación
+
+    //Agregar la tarea al state de tareas
+    task.projectId = actualProject.id;
+    task.stateTask = false;
+    addTaskFn(task);
+
+    //reiniciar el form
+  };
+
+  return (
+    <div className="formulario">
+      <form onSubmit={onSubmitAddTask}>
+        <div className="contenedor-input">
+          <input
+            type="text"
+            className="input-text"
+            placeholder="Nombre de la tarea"
+            name="nametask"
+            value={nametask}
+            onChange={hadleChange}
+          />
         </div>
-    );
+        <div className="contenedor-input">
+          <input
+            type="submit"
+            className="btn tbn-primario btn-submit btn-block"
+            value="Agregar tarea"
+          />
+        </div>
+      </form>
+    </div>
+  );
 };
 
 export default FormTask;
